dcdb3c515edb60914c91fd7102c47312c44b9bdb
lmy <mingyang.li@baozun.com>
2019-02-26 15:10:28 +0800

    fix bug, python replace python2

--- run_diff_my_image.bat
+++ run_diff_my_image.bat
@@ -1,1 +1,1 @@
-python2 ./dpxdt/tools/diff_my_images.py --upload_build_id=1 --release_server_prefix=http://localhost:5000/api --release_cut_url=http://example.com/path/to/my/release/tool/for/this/cut --casefile_path=./dpxdt/server/static/uploads/
+python ./dpxdt/tools/diff_my_images.py --upload_build_id=1 --release_server_prefix=http://localhost:5000/api --release_cut_url=http://example.com/path/to/my/release/tool/for/this/cut --casefile_path=./dpxdt/server/static/uploads/
